bb74613adf156e075bea72b626cdc7836d107632
zAeryos <dave_13_08@hotmail.it>
2024-03-17 19:38:55 +0100

    User ADMIN methods added.

--- src/main/java/it/epicode/capstonebe/controllers/UserController.java
+++ src/main/java/it/epicode/capstonebe/controllers/UserController.java
@@ -1,19 +1,20 @@
 package it.epicode.capstonebe.controllers;
 
 import com.cloudinary.Cloudinary;
-import it.epicode.capstonebe.exceptions.BadRequestException;
-import it.epicode.capstonebe.exceptions.HandlerException;
-import it.epicode.capstonebe.exceptions.InternalServerErrorException;
-import it.epicode.capstonebe.exceptions.UnauthorizedException;
+import it.epicode.capstonebe.exceptions.*;
 import it.epicode.capstonebe.models.entities.User;
 import it.epicode.capstonebe.models.requestDTO.PasswordUpdateDTO;
+import it.epicode.capstonebe.models.requestDTO.UserDTO;
 import it.epicode.capstonebe.models.requestDTO.UserUpdateDTO;
 import it.epicode.capstonebe.models.responseDTO.ConfirmRes;
 import it.epicode.capstonebe.models.responseDTO.DeleteRes;
 import it.epicode.capstonebe.security.JwtTools;
 import it.epicode.capstonebe.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -74,11 +75,43 @@ public class UserController {
         User user = userService.getById(userId);
         String username = user.getUsername();
         userService.delete(userId);
-        return new DeleteRes("User (username='" + username + "', id='" + userId + "') successfully deleted");
+        return new DeleteRes("User: '" + username + "', id: '" + userId + "' successfully deleted");
     }
 
     /*  ------------------------------ ADMIN ACCOUNT MODIFICATION ------------------------------  */
 
+    @GetMapping("/admin/getAll")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Page<User> getAllUsers(Pageable pageable) {
+        return userService.getAll(pageable);
+    }
 
+    @GetMapping("/admin/id")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User getById(@RequestParam UUID id) throws BadRequestException {
+        return userService.getById(id);
+    }
 
+    @PostMapping("/admin/save")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User saveUser(@RequestBody UserDTO userDTO, BindingResult bindingResult) throws NotFoundException, BadRequestException, InternalServerErrorException {
+        HandlerException.notFoundException(bindingResult);
+        return userService.save(userDTO);
+    }
+
+    @PatchMapping("/admin/update/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public User updateTrip(@PathVariable UUID id, @RequestBody UserUpdateDTO userDTO, BindingResult bindingResult) throws NotFoundException, BadRequestException, InternalServerErrorException {
+        HandlerException.notFoundException(bindingResult);
+        return userService.update(id, userDTO);
+    }
+
+    @DeleteMapping("/admin/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public DeleteRes deleteTrip(@PathVariable UUID id) throws NotFoundException, BadRequestException{
+
+        userService.delete(id);
+        return new DeleteRes("The user with id '" + id + "' has been successfully deleted");
+
+    }
 }
